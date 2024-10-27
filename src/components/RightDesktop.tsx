e7c90fec71341f6b321fed3a87d41c6d07cb6a8f
Wahyu Candra Tama <wahyucandratama@gmail.com>
2024-10-27 19:30:48 +0700

    Fix scroll on welcome page

--- src/components/RightDesktop.tsx
+++ src/components/RightDesktop.tsx
@@ -3,14 +3,17 @@ import {Box} from '@chakra-ui/react';
 import WelcomeSection from "@/components/WelcomeSection";
 import OpeningSection from "@/components/OpeningSection";
 
+interface RightDesktopProps {
+    isOverlayVisible: boolean;
+}
 
-const RightDesktop = ({}) => {
+const RightDesktop: React.FC<RightDesktopProps> = ({isOverlayVisible}) => {
     useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
 
     return (
-        <Box>
+        <Box className={!isOverlayVisible ? 'no-scroll' : ''}>
             <WelcomeSection/>
             <OpeningSection/>
         </Box>
